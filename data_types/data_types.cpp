ca5aec38d0e26edacfb323a548f63b5457cd88fb
alih3d <alih3d721@gmail.com>
2020-04-22 23:02:57 +0200

    "data types "

--- data_types/data_types.cpp
+++ data_types/data_types.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <complex>
+#include <string>
+#include <cassert>
+
 
 using namespace std;
 
@@ -16,10 +19,10 @@ int main(){
    cout << "float (a): " << float (a)/10. << endl;
 
 // double
-   cout << "size of double (a)in bytes: " << sizeof (double (a)) << endl;
+   cout << "size of double (a)in bytes: " << sizeof(double (a)) << endl;
 
 // long double - note the brackets around 'long double'
-   cout << "size of long double (a)in bytes: " << sizeof  ((long double) (a)) << endl;
+   cout << "size of long double (a)in bytes: " << sizeof((long double) (a)) << endl;
 
 // size of a byte in bits
    cout << "size of a byte in bits on computer using 'CHAR_BIT': " << CHAR_BIT << endl;
@@ -28,18 +31,38 @@ int main(){
    cout << "complex (a): " << complex<float> (a) << endl;
 
 // octal and hex 
+   cout << "oct (65): " << oct << 65 << endl;
+   cout << "hex (65): " << hex << 65 << endl;
 
 
 // char vs string
+   char b = 'a';
+  
+   const string c = "Hello";
+   char d[] = "    ";
 
-
+   cout << " 'a': " << b << "   '    ': " << '    ' << "  <-- multicharacter literal shows up as number" << endl;
+   cout << "'Hello': " << c << "   \"    \": " << d << " using double quotes will provide spaces"<< endl;
+  
 // boolean
+   cout << "bool (34): " << bool(34) << endl;
+   cout << "bool (1), (-1): " << bool (1) << " , " << bool (-1) << endl;
+   cout << "bool (0): " << bool (0) << endl;
 
-
-// type
-
-
+// type of
+   float e = 2.7;
+   char f = 'f';
+   char s[] = "hello";
+   const string cs = "hello";
+   cout << " use typeid( input ).name() for type info:" << endl;
+   cout << typeid(e).name() << "  "  << endl;
+   cout << typeid(f).name() << endl;
+   cout << typeid(s).name() << endl;
+   cout << typeid(cs).name() << endl;
+   
 // float operations
-
-
+   cout << " 1 / 10  : " << 1/10 << endl;
+   cout << " 1. / 10 : " << 1. / 10 << endl;
+   cout << "assert(1./10 == 1/10) : ";  assert(1./10 == 1/10);
+  
 }
--- data_types/data_types.py
+++ data_types/data_types.py
@@ -10,7 +10,7 @@ print ("sys.getsizeof(float(411.65): ",sys.getsizeof(float(411.65)))
 
 # more precision
 print("format(411.65,'.15f'): ",format(411.65,'.15f'))
-print("sys.getsizeof(format(411.65,'.15f')): ",sys.getsizeof(format(411.65,'.15f')))
+print("sys.getsizeof(format(411.65,'.15f')):" ,sys.getsizeof(format(411.65,'.15f')))
 
 # complex
 print("complex: ",complex(4, 5))
@@ -24,13 +24,14 @@ print("hex(411) : ", hex(411))
 # string
 print("string(411.65): ", str(411.65))
 
+
 # boolean
 print("bool(411.65): ", bool(411.65))
 print("bool(0): ", bool(0))
 print("bool('hello'): ", bool("hello"))
 print("bool(): ", bool())
 
-#type
+#type of
 print("type(411.65): ", type(411.65))
 print("type(str(411.65)): ", type(str(411.65)))
 
@@ -38,4 +39,4 @@ print("type(str(411.65)): ", type(str(411.65)))
 print("1/10 : ", 1/10)
 print("Decimal(1)/10 : ", Decimal(1)/10)
 print("Decimal(1) /10  == 1 /10 : ", Decimal(1) / 10 == 1/10 )  # gives False
-print("411.65%10 : ", 411.65%10)
+print("411.65 % 10 : ", 411.65%10)
